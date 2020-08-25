8512f283aeaab25bf4b39c9dd4ed4bb42a1f9151
pompom poto <48266828+jjoohee@users.noreply.github.com>
2020-08-26 04:42:30 +0900

    Add files via upload

---
+++ battle.js
@@ -0,0 +1,56 @@
+people1_4.onclick = function(){
+
+			people1_4.style.animation = "peo 0.3s infinite ease-in alternate";
+			people1_3.style.animation = "peo 0.3s 0.5s infinite ease-in alternate";
+			people1_2.style.animation = "peo 0.3s 1s infinite ease-in alternate";
+			people1_1.style.animation = "peo 0.3s 1.5s infinite ease-in alternate";
+		}
+
+people1_24.onclick = function(){
+
+			people1_24.style.animation = "peo 0.3s infinite ease-in alternate";
+			people1_23.style.animation = "peo 0.3s 0.5s infinite ease-in alternate";
+			people1_22.style.animation = "peo 0.3s 1s infinite ease-in alternate";
+			people1_21.style.animation = "peo 0.3s 1.5s infinite ease-in alternate";
+		}
+
+var  astate = "off";
+
+trophy.onclick = function(){
+			if (astate == "off") {
+			trophy.style.animation = "tro 2s ease";
+			trophy2.style.animation = "tro 2s ease";
+			trophy2.style.opacity = "100%";
+			trophy.style.opacity = "0%";
+			astate = "on";
+			}
+			else {
+			trophy.style.animation = "troo 2s ease";
+			trophy2.style.animation = "troo 2s ease";
+			trophy2.style.opacity = "0%";
+			trophy.style.opacity = "100%";
+			state = "off";
+			}
+		}
+
+
+//스크롤 작용
+window.onscroll = function(){
+		var t =document.documentElement.scrollTop + document.body.scrollTop;
+		moniter.innerHTML = t
+
+		if (t > 6600) {
+			people5_1arm.style.animation = "armmovefi 2s forwards";
+			people5_2arm.style.animation = "armmovefii 2s forwards";
+
+		}
+		else{
+			people5_1arm.style.animation = "";
+			people5_2arm.style.animation = "";
+		}
+
+		if (t > 6800) {
+			location.href = "https://jjoohee.github.io/misfortunevirus_0812/";
+		}
+}
+
