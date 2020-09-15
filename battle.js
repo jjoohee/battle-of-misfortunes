e4ba1610ba0834f319ab07051724b5b7d24f5ede
pompom poto <48266828+jjoohee@users.noreply.github.com>
2020-09-15 14:34:57 +0900

    Update battle.js

--- battle.js
+++ battle.js
@@ -1,49 +1,38 @@
-people1_4.onclick = function(){
-
-			people1_4.style.animation = "peo 0.3s infinite ease-in alternate";
-			people1_3.style.animation = "peo 0.3s 0.5s infinite ease-in alternate";
-			people1_2.style.animation = "peo 0.3s 1s infinite ease-in alternate";
-			people1_1.style.animation = "peo 0.3s 1.5s infinite ease-in alternate";
-		}
-
-people1_24.onclick = function(){
-
-			people1_24.style.animation = "peo 0.3s infinite ease-in alternate";
-			people1_23.style.animation = "peo 0.3s 0.5s infinite ease-in alternate";
-			people1_22.style.animation = "peo 0.3s 1s infinite ease-in alternate";
-			people1_21.style.animation = "peo 0.3s 1.5s infinite ease-in alternate";
-		}
-
-people4_1.onclick = function(){
-
-			talk.style.animation = "ta 0.1s infinite linear alternate, ta- 0.2s infinite linear alternate, bigger 0.3s linear";
-		}
-
-
-var  astate = "off";
-
-trophy.onclick = function(){
-			if (astate == "off") {
-			trophy.style.animation = "tro 2s ease";
-			trophy2.style.animation = "tro 2s ease";
-			trophy2.style.opacity = "100%";
-			trophy.style.opacity = "0%";
-			astate = "on";
-			}
-			else {
-			trophy.style.animation = "troo 2s ease";
-			trophy2.style.animation = "troo 2s ease";
-			trophy2.style.opacity = "0%";
-			trophy.style.opacity = "100%";
-			astate = "off";
-			}
-		}
-
 
 //스크롤 작용
 window.onscroll = function(){
 		var t =document.documentElement.scrollTop + document.body.scrollTop;
 		moniter.innerHTML = t
+
+			if (t > 800) {
+			people1_4.style.animation = "peo 0.3s infinite ease-in alternate";
+			people1_3.style.animation = "peo 0.3s 0.2s infinite ease-in alternate";
+			people1_2.style.animation = "peo 0.3s 0.4s infinite ease-in alternate";
+			people1_1.style.animation = "peo 0.3s 0.6s infinite ease-in alternate";
+
+		}
+
+		if (t > 1200) {
+			people1_24.style.animation = "peo 0.3s infinite ease-in alternate";
+			people1_23.style.animation = "peo 0.3s 0.2s infinite ease-in alternate";
+			people1_22.style.animation = "peo 0.3s 0.4s infinite ease-in alternate";
+			people1_21.style.animation = "peo 0.3s 0.6s infinite ease-in alternate";
+
+		}
+
+		if (t > 5300) {
+			trophy.style.animation = "tro 2s ease alternate";
+			trophy2.style.animation = "tro 2s ease alternate";
+			trophy2.style.opacity = "100%";
+			trophy.style.opacity = "0%";
+
+		}
+		else{
+			trophy.style.animation = "";
+			trophy2.style.animation = "";
+			trophy2.style.opacity = "";
+			trophy.style.opacity = "";
+		}
 
 		if (t > 7000) {
 			people5_1arm.style.animation = "armmovefi 2s forwards";
